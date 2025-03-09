7cb74f65ce2236b7287353a72eb4ad2e5c2a32a4
Tonia John <tonia@chariotmove.com>
2025-03-09 14:43:03 -0400

    bok

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -1,20 +1,30 @@
 import ParallaxSection from "@/components/animations/ParallaxSection";
 import BookingForm from "@/components/forms/BookingForm";
+import Hero from "@/components/layout/Hero";
+import { Button } from "@/components/ui/button";
+import { ArrowBigDownDash } from "lucide-react";
 
 import React from "react";
 
 function page() {
   return (
     <div className="mx-auto w-screen bg-cyan-950">
-      <ParallaxSection speed={-0.2} className="w-full h-full">
-        <div className="flex justify-center flex-col items-center h-screen space-y-4">
-          <h1 className="text-4xl text-center text-white pb-20">Reservations</h1>
-          <div>
-            <BookingForm />
+      <section className="relative h-screen flex items-center justify-center overflow-hidden">
+        <Hero image="/room-2.jpg" />
+        <ParallaxSection speed={-0.2}>
+          <h1 className="text-4xl text-center text-white pb-20 font-serif">Reservations</h1>
+          <div className="text-center">
+            <Button className="text-white bg-amber-500" asChild>
+              <a href="#booking-form" className="flex items-center justify-center">
+                Book Now <ArrowBigDownDash className="w-10 h-10 animate-bounce" />
+              </a>
+            </Button>
           </div>
-        </div>
-      </ParallaxSection>
-      {/* form with room, dates, get num guests and name email */}
+        </ParallaxSection>
+      </section>
+      <section className="py-24 px-4" id="booking-form">
+        <BookingForm />
+      </section>
     </div>
   );
 }
