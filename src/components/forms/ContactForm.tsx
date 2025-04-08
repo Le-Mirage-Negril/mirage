ebd4ca1621fc1da9640f7a27551b39a0093161ee
Tonia John <tonia@chariotmove.com>
2025-04-08 18:39:35 -0400

    remove comment

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -30,7 +30,7 @@ function ContactForm() {
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values);
-    // Handle form submission here
+
     const result = sendEmail(values, "template_ugb7z57");
     if (result) {
       form.reset();
