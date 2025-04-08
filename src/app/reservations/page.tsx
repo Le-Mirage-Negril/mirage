3f02e04cdf217f51069f5851b342e285bdb6ab44
Tonia John <tonia@chariotmove.com>
2025-04-08 18:33:28 -0400

    res page

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -27,7 +27,7 @@ function ReservationPage() {
       </section>
       <PageWrapper className="bg-cyan-950">
         <section id="booking-form">
-          <Typography variant="h2" className="text-center text-white mb-12">
+          <Typography variant="h2" className="text-center text-white pb-12">
             Book a Room
           </Typography>
           <Suspense
